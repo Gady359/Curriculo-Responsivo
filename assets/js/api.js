29beb199c767707ce0d3f3bad2a226835dac4930
Gady359 <gady.peters@gmail.com>
2024-02-06 17:00:24 -0300

    reforma header

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,7 @@
-async function fetchProfileData(){
-    const url ='https://raw.githubusercontent.com/Gady359/Curriculo-Responsivo/main/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/Gady359/Curriculo-Responsivo/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
